38c1f652863440b36c4064c7dd2fe5d9b3f12b23
Anson <ansfoong@gmail.com>
2022-09-03 21:25:09 -0700

    set group title to max length

--- src/components/groups/GroupSidebarItem.tsx
+++ src/components/groups/GroupSidebarItem.tsx
@@ -9,14 +9,31 @@ type Props = {
 };
 
 export const GroupSidebarItem: React.FC<Props> = ({ group }) => {
+  const MAX_TITLE_LENGTH = 20;
+  const MAX_MESSAGE_LENGTH = 50;
   const navigate = useNavigate();
+
+  const getTransformedTitle = () => {
+    if (!group.title) {
+      const usersToString = group.users
+        .map((user) => user.firstName)
+        .join(', ');
+      return usersToString.length > MAX_TITLE_LENGTH
+        ? usersToString.slice(0, MAX_TITLE_LENGTH).concat('...')
+        : usersToString;
+    }
+    return group.title.length > MAX_TITLE_LENGTH
+      ? group.title.slice(0, MAX_TITLE_LENGTH).concat('...')
+      : group.title;
+  };
+
   return (
     <ConversationSidebarItemStyle
       onClick={() => navigate(`/groups/${group.id}`)}
     >
       <div className={styles.groupAvatar}></div>
       <div>
-        <span className={styles.groupName}>{group.title || 'Group'}</span>
+        <span className={styles.groupName}>{getTransformedTitle()}</span>
         <span className={styles.groupLastMessage}>
           {group.lastMessageSent?.content}
         </span>
