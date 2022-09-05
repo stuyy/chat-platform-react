cded105b343ff823685a967eb4fc0d82c1bbd147
Anson <ansfoong@gmail.com>
2022-09-05 00:36:09 -0700

    fix sidebar width by preventing shrink

--- src/utils/styles/index.tsx
+++ src/utils/styles/index.tsx
@@ -528,6 +528,7 @@ export const ConversationSidebarStyle = styled.div`
   height: 100%;
   width: 400px;
   background-color: #111111;
+  flex: 0 0 auto;
 `;
 
 export const ConversationSidebarHeader = styled.header`
@@ -585,8 +586,9 @@ export const GroupRecipientsSidebarStyle = styled.aside`
   display: flex;
   flex-direction: column;
   height: 100%;
-  width: 500px;
+  width: 350px;
   background-color: #111111;
+  flex: 0 0 auto;
 `;
 
 export const GroupRecipientsSidebarHeader = styled.div`
