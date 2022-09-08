483af5581c3f8a3537e3b9a3b7f0a91573f1cf6c
Anson <ansfoong@gmail.com>
2022-09-07 21:09:32 -0700

    trim message before sending to api

--- src/components/messages/MessagePanel.tsx
+++ src/components/messages/MessagePanel.tsx
@@ -41,9 +41,11 @@ export const MessagePanel: FC<Props> = ({
   );
   const recipient = getRecipientFromConversation(conversation, user);
   const sendMessage = async () => {
-    if (!routeId || !content) return;
+    const trimmedContent = content.trim();
+    if (!routeId || !trimmedContent) return;
     const id = parseInt(routeId);
-    const params = { id, content };
+    const params = { id, content: trimmedContent };
+
     if (selectedType === 'private')
       return postNewMessage(params)
         .then(() => setContent(''))
