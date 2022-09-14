0f6430c0895cef7dec9daf7690d922c8f1580c23
Anson <ansfoong@gmail.com>
2022-09-13 21:08:49 -0700

    limit attachments to 5 and size to 1 MB

--- src/components/messages/MessageAttachmentActionIcon.tsx
+++ src/components/messages/MessageAttachmentActionIcon.tsx
@@ -6,6 +6,7 @@ import {
   addAttachment,
   incrementAttachmentCounter,
 } from '../../store/message-panel/messagePanelSlice';
+import { useToast } from '../../utils/hooks/useToast';
 import { FileInput } from '../../utils/styles/inputs/Textarea';
 import { DivMouseEvent, InputChangeEvent } from '../../utils/types';
 import styles from './index.module.scss';
@@ -14,7 +15,8 @@ export const MessageAttachmentActionIcon = () => {
   const attachmentIconRef = useRef<HTMLDivElement>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
   const dispatch = useDispatch<AppDispatch>();
-  const { attachmentCounter } = useSelector(
+  const { error } = useToast({ theme: 'dark' });
+  const { attachmentCounter, attachments } = useSelector(
     (state: RootState) => state.messagePanel
   );
 
@@ -25,7 +27,12 @@ export const MessageAttachmentActionIcon = () => {
 
   const onChange = (e: InputChangeEvent) => {
     const file = e.target.files?.item(0);
+    if (attachments.length >= 5)
+      return error('Maximum 5 Attachments Allowed', { position: 'top-center' });
+    if (file && file.size > 1000000)
+      return error('File exceeds limit: 1 MB', { position: 'top-center' });
     if (file) {
+      console.log(file);
       dispatch(addAttachment({ id: attachmentCounter, file }));
       dispatch(incrementAttachmentCounter());
     }
