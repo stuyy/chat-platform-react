0a796194553c7561e9e44befd80aaa678e363c9d
Anson <ansfoong@gmail.com>
2022-09-14 00:27:54 -0700

    render user avatar on UserSidebar or default if null

--- src/components/sidebars/UserSidebar.tsx
+++ src/components/sidebars/UserSidebar.tsx
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import { UserAvatar, UserSidebarStyle } from '../../utils/styles';
 import styles from './index.module.scss';
-import avatar from '../../__assets__/avatar_1.png';
+import avatar from '../../__assets__/default_avatar.jpg';
 import { CreateConversationModal } from '../modals/CreateConversationModal';
-import { userSidebarItems } from '../../utils/constants';
+import { CDN_URL, userSidebarItems } from '../../utils/constants';
 import { UserSidebarItem } from './items/UserSidebarItem';
+import { AuthContext } from '../../utils/context/AuthContext';
 
 export const UserSidebar = () => {
   const [showModal, setShowModal] = useState(false);
+  const { user } = useContext(AuthContext);
 
   return (
     <>
       {showModal && <CreateConversationModal setShowModal={setShowModal} />}
       <UserSidebarStyle>
-        <UserAvatar src={avatar} alt="avatar" width="55px" />
+        <UserAvatar
+          src={
+            user?.profile?.avatar
+              ? CDN_URL.concat(user?.profile.avatar)
+              : avatar
+          }
+          alt="avatar"
+          width="55px"
+        />
         <hr className={styles.hr} />
         {userSidebarItems.map((item) => (
           <UserSidebarItem item={item} />
