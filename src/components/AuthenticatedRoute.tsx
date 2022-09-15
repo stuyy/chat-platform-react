1f999bfb5f7a553c07a1c111468934ff491f395b
Isolated <isolatedbot@gmail.com>
2022-09-16 03:17:10 +0530

    added simple circle loading

--- src/components/AuthenticatedRoute.tsx
+++ src/components/AuthenticatedRoute.tsx
@@ -1,16 +1,17 @@
-import React, { FC } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
-import { useAuth } from '../utils/hooks/useAuth';
+import React, { FC } from 'react'
+import { Navigate, useLocation } from 'react-router-dom'
+import { useAuth } from '../utils/hooks/useAuth'
+import { CircleLoading } from './circle-loading'
 
 export const AuthenticatedRoute: FC<React.PropsWithChildren> = ({
   children,
 }) => {
-  const location = useLocation();
-  const { loading, user } = useAuth();
+  const location = useLocation()
+  const { loading, user } = useAuth()
 
   if (loading) {
-    return <div>loading</div>;
+    return <CircleLoading />
   }
-  if (user) return <>{children}</>;
-  return <Navigate to="/login" state={{ from: location }} replace />;
-};
+  if (user) return <>{children}</>
+  return <Navigate to="/login" state={{ from: location }} replace />
+}
