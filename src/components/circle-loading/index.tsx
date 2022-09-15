db71f81ddadb2601bdb7ee6cb3deef511c5180ce
Isolated <isolatedbot@gmail.com>
2022-09-15 17:16:58 +0530

    added circle loading

---
+++ src/components/circle-loading/index.tsx
@@ -0,0 +1,11 @@
+import "./index.css"
+export const CircleLoading = () => {
+    return <div className="loading">
+        <div className="spinner">
+            <div className="mask">
+                <div className="maskedCircle"></div>
+            </div>
+        </div>
+    </div>
+
+}
