b8f7fdba87ed39dbfecc9b17448f05214a495cb2
Anson <ansfoong@gmail.com>
2022-09-15 02:01:47 -0700

    add UserUpdateStatus modal when user clicks on their avatar on sidebar

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -17,6 +17,12 @@ export type Profile = {
   banner?: string;
 };
 
+export type UserPresence = {
+  id: number;
+  statusMessage?: string;
+  showOffline: boolean;
+};
+
 export type User = {
   id: number;
   username: string;
@@ -24,6 +30,7 @@ export type User = {
   firstName: string;
   lastName: string;
   profile?: Profile;
+  presence?: UserPresence;
 };
 
 export type Conversation = {
@@ -222,7 +229,11 @@ export type AcceptFriendRequestResponse = {
   friendRequest: FriendRequest;
 };
 
-export type UserSidebarRouteType = 'conversations' | 'friends' | 'connections' | 'settings';
+export type UserSidebarRouteType =
+  | 'conversations'
+  | 'friends'
+  | 'connections'
+  | 'settings';
 
 export type UserSidebarItemType = {
   id: UserSidebarRouteType;
