d54353c53924d5d764c9b9dbd5339c75abdf1f19
ARR4NN <70060472+ARR4NN@users.noreply.github.com>
2022-09-19 14:18:46 +0100

    cursor be nice

--- src/utils/styles/index.tsx
+++ src/utils/styles/index.tsx
@@ -716,6 +716,7 @@ export const IconBadge = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
 `;
 
 export const ConversationCreateButton = styled.div`
